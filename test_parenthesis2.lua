a823de9b3334436f636b81345f011621791e5600
Boris Nagaev <bnagaev@gmail.com>
2015-05-31 20:41:12 +0300

    add test_parenthesis2.lua

---
+++ test_parenthesis2.lua
@@ -0,0 +1,39 @@
+local lpeg = require 'lpeg'
+local lpeg2c = require 'lpeg2c'
+
+local function genParenthesis()
+    local abc = 'qwertyuiop[sdfghjklvbnmxcv56789\n'
+    local t = {}
+    math.randomseed(0)
+    for i = 1, 1000 do
+        table.insert(t, '(')
+    end
+    for i = 1, 2000000 do
+        if math.random(1, 2) == 1 then
+            table.insert(t, '(')
+        else
+            table.insert(t, ')')
+        end
+        if math.random(1, 2) == 1 then
+            local c = math.random(1, #abc)
+            table.insert(t, abc:sub(c, c))
+        end
+    end
+    return table.concat(t)
+end
+
+local pattern = lpeg.P {
+    "(" * ((1 - lpeg.S"()") + lpeg.V(1))^0 * ")"
+}
+
+local pattern2 = lpeg2c(pattern)
+local text = genParenthesis()
+local t1 = os.clock()
+local res = pattern2(text)
+local t2 = os.clock()
+print('c', res, t2 - t1)
+local t1 = os.clock()
+local check = text:match('%b()')
+local t2 = os.clock()
+print('lua', #check, t2 - t1)
+assert(res == #check + 1)
