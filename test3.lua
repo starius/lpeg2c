a03c3809fc1259e0f2cd0575a778f621d53ccd96
Boris Nagaev <bnagaev@gmail.com>
2015-05-31 17:42:49 +0300

    test3: add pattern "balanced parenthesis"

--- test3.lua
+++ test3.lua
@@ -1,6 +1,12 @@
 local lpeg = require 'lpeg'
-local pattern = lpeg.P 'abc'
-
 local lpeg2c = require 'lpeg2c'
+
+local pattern = lpeg.P 'abc'
 local pattern2 = lpeg2c(pattern)
 assert(pattern2('abc') == 4)
+
+local pattern = lpeg.P{
+    "(" * (lpeg.V(1))^0 * ")"
+}
+local pattern2 = lpeg2c(pattern)
+assert(pattern2('(())') == 5)
